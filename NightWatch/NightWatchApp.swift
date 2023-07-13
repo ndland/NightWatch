cd83ec4b2050e6ce9d1b8b5d1d99d070ad7229a1
Nick Land <land.d.nicholas@gmail.com>
2023-07-13 19:29:44 -0400

    Initial Commit

---
+++ NightWatch/NightWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  NightWatchApp.swift
+//  NightWatch
+//
+//  Created by Nicholas Land on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct NightWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
