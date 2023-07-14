9751c7ed4dabea3fc990606b81be6ce14d2c0da0
Nick Land <land.d.nicholas@gmail.com>
2023-07-13 20:42:14 -0400

    feat: update ContentView to include initial nightly tasks
    * Leave off at Building Single View Applications -> Developing a SwiftUI Mindset in iOS 14 Getting Started course on Pluralsight

--- NightWatch/ContentView.swift
+++ NightWatch/ContentView.swift
@@ -10,12 +10,38 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Nightly Tasks")
+                        .underline()
+                        .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
+                        .fontWeight(/*@START_MENU_TOKEN@*/.heavy/*@END_MENU_TOKEN@*/)
+                        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                    Text("Check all windows")
+                    Text("Check all doors")
+                    Text("Check that the safe is locked")
+                    Text("Inspect security cameras")
+                    Text("Clear ice from sidewalks")
+                    Text("Document \"strange and unusual\" occurances")
+                    Text("Weekly Tasks")
+                        .underline()
+                        .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
+                        .fontWeight(/*@START_MENU_TOKEN@*/.heavy/*@END_MENU_TOKEN@*/)
+                        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                        .padding(.top)
+                    Text("Monthly Tasks")
+                        .underline()
+                        .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
+                        .fontWeight(/*@START_MENU_TOKEN@*/.heavy/*@END_MENU_TOKEN@*/)
+                        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                        .padding(.top)
+                }
+                .foregroundColor(/*@START_MENU_TOKEN@*/.gray/*@END_MENU_TOKEN@*/)
+                Spacer()
+            }
+            .padding([.top, .leading])
+            Spacer()
         }
-        .padding()
     }
 }
 
