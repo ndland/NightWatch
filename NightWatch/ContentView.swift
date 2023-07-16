504bb4ff923de5dbe6a036a235c17ba14ee730c2
Nick Land <land.d.nicholas@gmail.com>
2023-07-15 20:09:00 -0400

    feat(module 4): finish module 4
    The app is now using ListView to display the tasks.
    However, it is not truly functional yet, as the list view
    does nothing more than just show the tasks.

--- NightWatch/ContentView.swift
+++ NightWatch/ContentView.swift
@@ -4,85 +4,64 @@
 //
 //  Created by Nicholas Land on 7/13/23.
 //
-
 import SwiftUI
+
+let nightlyTasks = [
+    "Check all windows",
+    "Check all doors",
+    "Check that the safe is locked",
+    "Inspect security cameras",
+    "Clear ice from sidewalks",
+    "Document \"strange and unusual\" occurances"
+]
+
+let weeklyTasks = [
+    "Check inside all vacant rooms",
+    "Walk the permimeter of the property"
+]
+
+let monthlyTasks = [
+    "Test security alerm",
+    "Test motion detectors",
+    "Test smoke alerms"
+]
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            // MARK: Nightly Tasks
-            HStack {
-                VStack(alignment: .leading) {
-                    Group {
-                        HStack {
-                            Text(Image(systemName: "moon.stars"))
-                                .foregroundColor(.yellow)
-                                .font(.title3)
-                                .fontWeight(.heavy)
-                            Text("Nightly Tasks")
-                                .underline()
-                                .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
-                                .fontWeight(/*@START_MENU_TOKEN@*/.heavy/*@END_MENU_TOKEN@*/)
-                                .foregroundColor(/*@START_MENU_TOKEN@*/.yellow/*@END_MENU_TOKEN@*/)
-                                .textCase(.uppercase)
-                        }
-                        
-                        Text("- Check all windows")
-                        Text("- Check all doors")
-                        Text("- Check that the safe is locked")
-                        Text("- Inspect security cameras")
-                        Text("- Clear ice from sidewalks")
-                        Text("- Document \"strange and unusual\" occurances")
-                    }
-                    
-                    // MARK: Weekly Tasks
-                    Divider()
-                    Group {
-                        HStack {
-                            Text(Image(systemName: "sunset"))
-                                .foregroundColor(.yellow)
-                                .font(.title3)
-                                .fontWeight(.heavy)
-                            Text("Weekly Tasks")
-                                .underline()
-                                .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
-                                .fontWeight(/*@START_MENU_TOKEN@*/.heavy/*@END_MENU_TOKEN@*/)
-                                .foregroundColor(/*@START_MENU_TOKEN@*/.yellow/*@END_MENU_TOKEN@*/)
-                                .padding(.top)
-                                .textCase(.uppercase)
-                        }
-                    }
-                    
-                    Text("- Check inside all vacant rooms")
-                    Text("- Walk the permimeter of the property")
-                    // MARK: Monthly Tasks
-                    Divider()
-                    Group {
-                        HStack {
-                            Text(Image(systemName: "calendar"))
-                                .foregroundColor(.yellow)
-                                .font(.title3)
-                                .fontWeight(.heavy)
-                            Text("Monthly Tasks")
-                                .underline()
-                                .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
-                                .fontWeight(/*@START_MENU_TOKEN@*/.heavy/*@END_MENU_TOKEN@*/)
-                                .foregroundColor(/*@START_MENU_TOKEN@*/.yellow/*@END_MENU_TOKEN@*/)
-                                .padding(.top)
-                                .textCase(.uppercase)
-                        }
-                    }
-                    
-                    Text("- Test security alerm")
-                    Text("- Test motion detectors")
-                    Text("- Test smoke alerms")
-                }
-                .foregroundColor(/*@START_MENU_TOKEN@*/.gray/*@END_MENU_TOKEN@*/)
-                Spacer()
+        List {
+            Section(header: HStack {
+                Image(systemName: "moon.stars")
+                Text("Nightly Tasks")
             }
-            .padding([.top, .leading])
-            Spacer()
-        }
+                .font(.title3)) {
+                    ForEach(nightlyTasks, id: \.self, content: {
+                        nightlyTask in
+                        Text(nightlyTask)
+                    })
+                }
+            
+            Section(header: HStack {
+                Image(systemName: "sunset")
+                Text("Weekly Tasks")
+            }
+                .font(.title3)) {
+                    ForEach(weeklyTasks, id: \.self, content: {
+                        weeklyTask in
+                        Text(weeklyTask)
+                    })
+                }
+            
+            Section(header: HStack {
+                Image(systemName: "calendar")
+                Text("Monthly Tasks")
+            }
+                .font(.title3)) {
+                    ForEach(monthlyTasks, id: \.self, content: {
+                        monthlyTask in
+                        Text(monthlyTask)
+                    })
+                }
+        }.listStyle(GroupedListStyle())
     }
 }
 
